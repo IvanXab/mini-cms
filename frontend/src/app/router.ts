67b9df8c17f114a671f9fbc4382a03ef2a41d22a
IvanXab <ivanxab54@gmail.com>
2026-02-22 21:39:15 +0700

    feat(router): Добавлена страница для создания/редактирования публикаций
    Signed-off-by: IvanXab <ivanxab54@gmail.com>

--- frontend/src/app/router.ts
+++ frontend/src/app/router.ts
@@ -1,14 +1,25 @@
 import { createWebHistory, createRouter } from "vue-router";
-import { RoutesName } from "@/shared/common/routes.const.ts";
+import {RoutesName, RoutesTitle} from "@/shared/common/routes.const.ts";
 const MainPage = () => import("@/pages/main-page/MainPage.vue");
+const ArticleEditorPage = () => import("@/pages/article-editor-page/ArticleEditorPage.vue");
 
 const routes = [
   {
     path: "/",
     name: RoutesName.MAIN,
     component: MainPage,
+    meta: {
+      title: RoutesTitle.MAIN
+    },
     children: [
-
+      {
+        path: "/article-editor",
+        name: RoutesName.ARTICLE_EDITOR,
+        component: ArticleEditorPage,
+        meta: {
+          title: RoutesTitle.ARTICLE_EDITOR
+        },
+      }
     ]
   },
 ];
--- frontend/src/shared/common/routes.const.ts
+++ frontend/src/shared/common/routes.const.ts
@@ -1,7 +1,9 @@
 export const RoutesName = {
   MAIN: 'MainPage',
+  ARTICLE_EDITOR: 'ArticleEditorPage',
 } as const;
 
 export const RoutesTitle = {
   MAIN: 'Главная страница',
+  ARTICLE_EDITOR: 'Создание публикации'
 } as const;
