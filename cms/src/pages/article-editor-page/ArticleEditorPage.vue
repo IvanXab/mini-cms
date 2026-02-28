d814f38b2ba087e00cff748642adea3098e719d9
IvanXab <ivanxab54@gmail.com>
2026-02-28 22:09:07 +0700

    chore(struct): Обновление структуры проекта
    Signed-off-by: IvanXab <ivanxab54@gmail.com>

---
+++ cms/src/pages/article-editor-page/ArticleEditorPage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="article-editor-page">
+    <EditorContent class="article-editor-page__editor" :editor />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { EditorContent, useEditor } from "@tiptap/vue-3";
+import StarterKit from "@tiptap/starter-kit";
+
+const editor = useEditor({
+  extensions: [StarterKit],
+  content: '',
+  autofocus: true,
+  editable: true,
+});
+</script>
+
+<style scoped lang="scss">
+.article-editor-page {
+  padding: 40px;
+
+  &__editor {
+    border: 1px solid #ddd;
+    min-height: 400px;
+
+    &:deep(.tiptap) {
+      height: 100%;
+    }
+  }
+}
+</style>
